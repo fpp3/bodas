ab3f2ab32d958ab67f76f1492f149af14418d331
Franco Palombo <ffpp.2003@hotmail.com>
2026-05-31 20:26:47 -0300

    splits graphs in 4 different figures

--- bodas.m
+++ bodas.m
@@ -87,264 +87,231 @@ omega = logspace(wmin, wmax, 5000);
 s = tf('s');
 
 % -------------------------------------------------------------------------
-% Gain plot
+% Data calculation
 % -------------------------------------------------------------------------
 
-figure;
-set(gcf,'units','normalized','outerposition',[0 0 1 1])
-
-[ha, ] = tight_subplot(1, 2, [0.05, 0.05]);
-
-axes(ha(1));
-hold on;
-grid on;
-
-legend_text = cell(length(z) + length(p) + 2 + 1, 1);
-ctr = 1;
-
-A = zeros(length(z), length(omega));
-B = zeros(length(p), length(omega));
-C = zeros(1, length(omega));
+% Gain data
+A_gain = zeros(length(z), length(omega));
+B_gain = zeros(length(p), length(omega));
+C_gain = zeros(1, length(omega));
+legend_text_gain = {};
 
 G = 1;
-
 for i = 1:length(z)
     if z(i) == 0 % Zero at the origin
-        A(i,:) = 20*log10(omega);
+        A_gain(i,:) = 20*log10(omega);
+        legend_text_gain{end+1} = "s";
+        G = G * s;
     elseif isreal(z(i)) == true
         for j = 1:length(omega)
             if omega(j) >= z(i)
-                A(i,j) = 20*log10(omega(j)/z(i));
+                A_gain(i,j) = 20*log10(omega(j)/z(i));
             else
-                A(i,j) = 0; % Arranca en 0 dB
+                A_gain(i,j) = 0;
             end
         end
-    elseif isreal(z(i)) == false
+        legend_text_gain{end+1} = ["(1 + s/", num2str(z(i)), ")"];
+        G = G * (1 + s/z(i));
+    else
         peak = false;
         wn = sqrt(z(i)*conj(z(i)));
         zeta = (z(i)+conj(z(i)))/2/wn;
         for j = 1:length(omega)
             if omega(j) >= wn
-                A(i,j) = 40*log10(omega(j)/wn);
+                A_gain(i,j) = 40*log10(omega(j)/wn);
                 if peak == false
                     peak_idx = j;
                     peak = true;
                 end
             else
-                A(i,j) = 0; % Arranca en 0 dB
+                A_gain(i,j) = 0;
             end
         end
         if zeta < 0.5 && peak == true
-            A(i,peak_idx) = A(i,peak_idx) - 20*log10(1/(2*zeta));
+            A_gain(i,peak_idx) = A_gain(i,peak_idx) - 20*log10(1/(2*zeta));
         end
+        legend_text_gain{end+1} = ["(1 + s/", num2str(z(i)), ")(1 + s/", num2str(conj(z(i))), ")"];
+        G = G * (1 + s/z(i)) * (1 + s/conj(z(i)));
     end
-    
-    plot(omega,A(i,:), 'LineWidth', 2);
-    if z(i) == 0
-        legend_text{ctr} = "s";
-        G = G * s;
-    else
-        if isreal(z(i)) == true
-            legend_text{ctr} = ["(1 + s/", num2str(z(i)), ")"];
-            G = G * (1 + s/z(i));
-        elseif isreal(z(i)) == false
-            legend_text{ctr} = ["(1 + s/", num2str(z(i)), ")(1 + s/", num2str(conj(z(i))), ")"];
-            G = G * (1 + s/z(i)) * (1 + s/conj(z(i)));
-        end
-    end
-    ctr = ctr + 1;
 end
 
 for i = 1:length(p)
     if p(i) == 0  % Pole at the origin
-        B(i,:) = -20*log10(omega);
+        B_gain(i,:) = -20*log10(omega);
+        legend_text_gain{end+1} = "1/s";
+        G = G * 1/s;
     elseif isreal(p(i)) == true
         for j = 1:length(omega)
             if omega(j) >= p(i)
-                B(i,j) = -20*log10(omega(j)/p(i));
+                B_gain(i,j) = -20*log10(omega(j)/p(i));
             else
-                B(i,j) = 0; % Arranca en 0 dB
+                B_gain(i,j) = 0;
             end
         end
-    elseif isreal(p(i)) == false
+        legend_text_gain{end+1} = ["1/(1 + s/", num2str(p(i)), ")"];
+        G = G * 1/(1 + s/p(i));
+    else
         peak = false;
         wn = sqrt(p(i)*conj(p(i)));
         zeta = (p(i)+conj(p(i)))/2/wn;
         for j = 1:length(omega)
             if omega(j) >= wn
-                B(i,j) = -40*log10(omega(j)/wn);
+                B_gain(i,j) = -40*log10(omega(j)/wn);
                 if peak == false
                     peak_idx = j;
                     peak = true;
                 end
             else
-                B(i,j) = 0; % Arranca en 0 dB
+                B_gain(i,j) = 0;
             end
         end
         if zeta < 0.5 && peak == true
-            B(i,peak_idx) = B(i,peak_idx) + 20*log10(1/(2*zeta));
+            B_gain(i,peak_idx) = B_gain(i,peak_idx) + 20*log10(1/(2*zeta));
         end
-    end
-    
-    
-    plot(omega,B(i,:), 'LineWidth', 2);
-    if p(i) == 0
-        legend_text{ctr} = "1/s";
-        G = G * 1/s;
-    elseif isreal(p(i)) == false
-        legend_text{ctr} = ["1/( (1+s/", num2str(p(i)), ")(1+s/", num2str(conj(p(i))), ") )"];
+        legend_text_gain{end+1} = ["1/( (1+s/", num2str(p(i)), ")(1+s/", num2str(conj(p(i))), ") )"];
         G = G * 1/( (1+s/p(i)) * (1+s/conj(p(i))) );
-    elseif isreal(p(i)) == true
-        legend_text{ctr} = ["1/(1 + s/", num2str(p(i)), ")"];
-        G = G * 1/(1 + s/p(i));
     end
-    ctr = ctr + 1;
 end
 
-% The gain (Modificado para graficar la constante K_norm)
 if K_norm ~= 0
     kDb = 20*log10(abs(K_norm));
-    for j = 1:length(omega)
-        C(j) = kDb;
-    end
-    
-    plot(omega, C, 'LineWidth', 2);
-    legend_text{ctr} = ['K = ', num2str(K_norm)];
+    C_gain(:) = kDb;
+    legend_text_gain{end+1} = ['K = ', num2str(kDb), ' dB'];
     G = G * K_norm;
-    ctr = ctr + 1;
 end
 
-[mag,phase, wout] = bode(G, {10^wmin, 10^wmax});       
+[mag, phase, wout] = bode(G, {10^wmin, 10^wmax});
 phase = squeeze(phase);
 mag = squeeze(mag);
+M = sum([sum(A_gain,1); sum(B_gain,1); C_gain], 1);
 
-% Combinations
-M = sum([sum(A,1);sum(B,1);C],1);
-plot(omega, M, 'LineWidth',3, 'LineStyle', '-', 'Color', [.7, .7,.7]);
-plot(wout, 20*log10(mag), 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
-
-legend_text{end-1} = "Asymptotic Bode";
-legend_text{end}= "Exact Bode";
-legend(legend_text(~cellfun('isempty',legend_text)), 'Location', 'best');
-ylabel('Magnitude (dB)');
-set(gca, 'XScale', 'log');
-
-% -------------------------------------------------------------------------
-% Phase plot
-% -------------------------------------------------------------------------
-
-axes(ha(2));
-hold on;
-grid on;
-
-A = zeros(1,length(omega));
-B = zeros(1,length(omega));
-C = zeros(1,length(omega));
+% Phase data
+A_phase = zeros(length(z), length(omega));
+B_phase = zeros(length(p), length(omega));
+C_phase = zeros(1, length(omega));
+legend_text_phase = {};
 
 for i = 1:length(z)
     if z(i) == 0
-        A(i,:)=ones(1,length(omega))*90;
+        A_phase(i,:) = 90;
+        legend_text_phase{end+1} = "s";
     elseif isreal(z(i)) == true
         for j = 1:length(omega)
-            if omega(j) > z(i)/10 && omega(j) <= z(i)*10
-                A(i,j)=min(45*log10(omega(j)/omega(j-1))+ A(i,j-1),90);
+            if j > 1 && omega(j) > z(i)/10 && omega(j) <= z(i)*10
+                A_phase(i,j) = min(45*log10(omega(j)/omega(j-1)) + A_phase(i,j-1), 90);
             elseif omega(j) <= z(i)/10
-                A(i,j)=0;
+                A_phase(i,j) = 0;
             elseif omega(j) > z(i)*10
-                A(i,j)=90;
+                A_phase(i,j) = 90;
             end
         end
-    elseif isreal(z(i)) == false
+        legend_text_phase{end+1} = ["(1 + s/", num2str(z(i)), ")"];
+    else
         wn = sqrt(z(i)*conj(z(i))); 
         zeta = (z(i)+conj(z(i)))/2/wn;
         delta = 10^zeta;
-
         for j = 1:length(omega)
-            if (omega(j) > wn/delta) && (omega(j) <= wn*delta)
-                A(i,j)=90/zeta*log10(omega(j)/omega(j-1))+ A(i,j-1);
+            if j > 1 && (omega(j) > wn/delta) && (omega(j) <= wn*delta)
+                A_phase(i,j) = 90/zeta*log10(omega(j)/omega(j-1)) + A_phase(i,j-1);
             elseif omega(j) <= wn/delta
-                A(i,j)=0;
+                A_phase(i,j) = 0;
             elseif omega(j) > wn*delta
-                A(i,j)=180;
+                A_phase(i,j) = 180;
             end
         end
+        legend_text_phase{end+1} = ["(1 + s/", num2str(z(i)), ")(1 + s/", num2str(conj(z(i))), ")"];
     end
-    plot(omega,A(i,:), 'LineWidth', 2);
 end
 
 for i = 1:length(p)
     if p(i) == 0
-        B(i,:)=ones(1,length(omega))*-90;
+        B_phase(i,:) = -90;
+        legend_text_phase{end+1} = "1/s";
     elseif isreal(p(i)) == true
         for j = 1:length(omega)
-            if omega(j) > p(i)/10 && omega(j) <= p(i)*10
-                B(i,j)=max(-45*log10(omega(j)/omega(j-1))+ B(i,j-1), -90);
+            if j > 1 && omega(j) > p(i)/10 && omega(j) <= p(i)*10
+                B_phase(i,j) = max(-45*log10(omega(j)/omega(j-1)) + B_phase(i,j-1), -90);
             elseif omega(j) <= p(i)/10
-                B(i,j)=0;
+                B_phase(i,j) = 0;
             elseif omega(j) > p(i)*10
-                B(i,j)=-90;
+                B_phase(i,j) = -90;
             end
         end
-     elseif isreal(p(i)) == false
+        legend_text_phase{end+1} = ["1/(1 + s/", num2str(p(i)), ")"];
+    else
         wn = sqrt(p(i)*conj(p(i))); 
         zeta = (p(i)+conj(p(i)))/2/wn;
         delta = 10^zeta;
-
         for j = 1:length(omega)
-            if (omega(j) > wn/delta) && (omega(j) <= wn*delta)
-                B(i,j)=-90/zeta*log10(omega(j)/omega(j-1))+ B(i,j-1);
+            if j > 1 && (omega(j) > wn/delta) && (omega(j) <= wn*delta)
+                B_phase(i,j) = -90/zeta*log10(omega(j)/omega(j-1)) + B_phase(i,j-1);
             elseif omega(j) <= wn/delta
-                B(i,j)=0;
+                B_phase(i,j) = 0;
             elseif omega(j) > wn*delta
-                B(i,j)=-180;
+                B_phase(i,j) = -180;
             end
         end
-    end    
-    plot(omega,B(i,:), 'LineWidth', 2);
-end
-
-% The gain phase (Modificado para detectar el signo de K_norm)
-if K_norm ~= 0
-    for j = 1:length(omega)
-        if (K_norm > 0)
-            C(j)=0;
-        else
-            C(j)=-180;
-        end
+        legend_text_phase{end+1} = ["1/( (1+s/", num2str(p(i)), ")(1+s/", num2str(conj(p(i))), ") )"];
     end
-    plot(omega, C, 'LineWidth', 2);
 end
 
-% Combinations
-S = sum([sum(A,1);sum(B,1);C],1);
+if K_norm ~= 0
+    if K_norm > 0
+        C_phase(:) = 0;
+    else
+        C_phase(:) = -180;
+    end
+    legend_text_phase{end+1} = ['K = ', num2str(K_norm)];
+end
+
+S = sum([sum(A_phase,1); sum(B_phase,1); C_phase], 1);
 dphase = S(end) - phase(end);
 
-plot(omega, S, 'LineWidth',3, 'LineStyle', '-', 'Color', [.7, .7,.7]);
-plot(wout, phase+dphase, 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
-ylabel('Phase (degrees)')
-xlabel('Frequency (log scale)')
-set(gca, 'XScale', 'log');
+% -------------------------------------------------------------------------
+% Plotting
+% -------------------------------------------------------------------------
 
-figure;
-[hb, ] = tight_subplot(2, 1, [0.05, 0.05]);
-
-axes(hb(1));
-hold on;
-grid on;
-plot(omega, M, 'LineWidth',3, 'LineStyle', '-', 'Color', [.7, .7,.7]);
+% Figure 1: Gain with parts
+figure('Name', 'Magnitude: Asymptotic with Parts', 'NumberTitle', 'off');
+set(gcf,'units','normalized','outerposition',[0 0.5 0.5 0.5])
+hold on; grid on;
+for i = 1:size(A_gain, 1), plot(omega, A_gain(i,:), 'LineWidth', 1.5); end
+for i = 1:size(B_gain, 1), plot(omega, B_gain(i,:), 'LineWidth', 1.5); end
+if K_norm ~= 0, plot(omega, C_gain, 'LineWidth', 1.5); end
+plot(omega, M, 'LineWidth', 3, 'Color', [.7, .7, .7]);
 plot(wout, 20*log10(mag), 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
-legend({'Asymptotic Bode', 'Exact Bode'}, 'Location', 'best');
-ylabel('Magnitude (dB)');
-set(gca, 'XScale', 'log');
+legend([legend_text_gain, "Asymptotic Sum", "Exact Bode"], 'Location', 'best');
+ylabel('Magnitude (dB)'); xlabel('Frequency (rad/s)'); set(gca, 'XScale', 'log');
 
-axes(hb(2));
-hold on;
-grid on;
-plot(omega, S, 'LineWidth',3, 'LineStyle', '-', 'Color', [.7, .7,.7]);
-plot(wout, phase+dphase, 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
-ylabel('Phase (degrees)')
-xlabel('Frequency (log scale)')
-set(gca, 'XScale', 'log');
+% Figure 2: Gain only
+figure('Name', 'Magnitude: Asymptotic only', 'NumberTitle', 'off');
+set(gcf,'units','normalized','outerposition',[0.5 0.5 0.5 0.5])
+hold on; grid on;
+plot(omega, M, 'LineWidth', 3, 'Color', [.7, .7, .7]);
+plot(wout, 20*log10(mag), 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
+legend({"Asymptotic Sum", "Exact Bode"}, 'Location', 'best');
+ylabel('Magnitude (dB)'); xlabel('Frequency (rad/s)'); set(gca, 'XScale', 'log');
 
-w ={10^wmin 10^wmax};
+% Figure 3: Phase with parts
+figure('Name', 'Phase: Asymptotic with Parts', 'NumberTitle', 'off');
+set(gcf,'units','normalized','outerposition',[0 0 0.5 0.5])
+hold on; grid on;
+for i = 1:size(A_phase, 1), plot(omega, A_phase(i,:), 'LineWidth', 1.5); end
+for i = 1:size(B_phase, 1), plot(omega, B_phase(i,:), 'LineWidth', 1.5); end
+if K_norm ~= 0, plot(omega, C_phase, 'LineWidth', 1.5); end
+plot(omega, S, 'LineWidth', 3, 'Color', [.7, .7, .7]);
+plot(wout, phase + dphase, 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
+legend([legend_text_phase, "Asymptotic Sum", "Exact Bode"], 'Location', 'best');
+ylabel('Phase (degrees)'); xlabel('Frequency (rad/s)'); set(gca, 'XScale', 'log');
+
+% Figure 4: Phase only
+figure('Name', 'Phase: Asymptotic only', 'NumberTitle', 'off');
+set(gcf,'units','normalized','outerposition',[0.5 0 0.5 0.5])
+hold on; grid on;
+plot(omega, S, 'LineWidth', 3, 'Color', [.7, .7, .7]);
+plot(wout, phase + dphase, 'LineWidth', 2, 'LineStyle', ':', 'Color', 'k');
+legend({"Asymptotic Sum", "Exact Bode"}, 'Location', 'best');
+ylabel('Phase (degrees)'); xlabel('Frequency (rad/s)'); set(gca, 'XScale', 'log');
+
+w = {10^wmin 10^wmax};
 end
